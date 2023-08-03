3c9197188d68a630dd37a5e474db173fe65c8323
Ivan <ivanesptic2@gmail.com>
2023-08-03 18:49:43 +0200

    put code to create grid in a function

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 const container = document.querySelector("#container");
+createGrid(prompt("Number of squares"))
 
-for(let i= 1; i<=16; i++){
-    const gridDivs = document.createElement("div");
-    container.appendChild(gridDivs);
-    gridDivs.className = "outerDiv";
-
-    for(let j = 1; j<=16;j++){
-        const innerDivs = document.createElement("div");
-        innerDivs.className = "innerDiv";
-        gridDivs.appendChild(innerDivs);
+function createGrid(squares){
+    for(let i= 0; i<squares; i++){
+        const gridDivs = document.createElement("div");
+        container.appendChild(gridDivs);
+        gridDivs.className = "outerDiv";
+    
+        for(let j = 0; j<squares;j++){
+            const innerDivs = document.createElement("div");
+            innerDivs.className = "innerDiv";
+            innerDivs.style.width = `${squares}px`
+            innerDivs.style.height = `${squares}px`
+    
+            innerDivs.id = "innerDiv";
+            gridDivs.appendChild(innerDivs);
+        }
     }
+
 }
+
+
+const innerDivSelector = document.querySelectorAll("#innerDiv");
+innerDivSelector.forEach((div) => {
+    div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = "black"
+    });
+  }); 
